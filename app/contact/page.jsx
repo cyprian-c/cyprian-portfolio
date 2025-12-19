340533f7d25a1dd4104a74284d8f0ff440e6cb32
Cyprian <ocharo34@gmail.com>
2025-12-19 16:23:47 +0000

    tweaks

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,80 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+    {
+        icon: <FaPhoneAlt />,
+        title: "Phone",
+        description: "(+254) 788 523 896",
+    },
+     {
+        icon: <FaEnvelope />,
+        title: "Email",
+        description: "ocharo.dev@gmail.com",
+    },
+     {
+        icon: <FaMapMarkerAlt />,
+        title: "Address",
+        description: "125, Ongata Rongai",
+    },
+];
+
+import { motion } from "framer-motion";
 const Contact = () => {
     return (
-        <div>Contact Page</div>
-    )
+        <motion.section
+
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="py-6"
+        >
+          <div className="container mx-auto">
+            <div className="flex flex-col xl:flex-row gap-[30px]">
+                {/* form */}
+                <div className="xl:h-[54%] order-2 xl:order-none">
+                    <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                       <h3 className="text-4xl text-accent">Let's work together</h3>
+                       <p className="text-white/60">
+                         lorem ipsum this is a test for the fng website
+                       </p>
+                       {/* input */}
+                       <div className="grid grid-cols-1
+                       md:grid-cols-2 gap-6">
+                        <Input type="firstname" placeholder="Firstname" />
+                        <Input type="lastname" placeholder="Lastname" />
+                        <Input type="email" placeholder="Email address" />
+                        <Input type="phone" placeholder="Phone number" />
+                       </div>
+                    </form>
+                </div>
+                {/* info */}
+                <div className="flex-1 flex items-center xl:justify-end order-1
+                xl:order-none mb-8 xl:mb-0">
+                    info
+                </div>
+            </div>
+          </div>
+            
+        </motion.section>
+    );
 };
 
 export default Contact;
